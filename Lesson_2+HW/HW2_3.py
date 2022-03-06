b149d3d8c02a9d29308f6b1b0f4a95ce292a530e
milash1 <ludmila.chitik@gmail.com>
2022-03-06 11:57:40 +0300

    Delete HW2_3.py

--- Lesson_2+HW/HW2_3.py
+++
@@ -1,8 +0,0 @@
-while True:
-  user_month = input("Введите номер месяца от 1 до 12 - ")
-  if user_month.isdigit() and 0 < int(user_month) <= 12:
-    season = {0: 'зима', 1: ' весна', 2: 'лето', 3: 'осень', 4: 'зима'}
-    print(f'список сезонов - {season [int(user_month) // 3]}')
-    break
-else:
-    print('Ошибка')
