f04b3632a91f0a62e3669aeecb4550818507587b
milash1 <ludmila.chitik@gmail.com>
2022-03-06 11:21:34 +0300

    3-е задание не смогла выполнить самостоятельно. В PC ошибка, подскажите плз что я натворила:C:\PyProjects\Project_1\venv\Scripts\python.exe C:/PyProjects/Project_1/main.py C:\Users\Xiaomi\AppData\Local\Programs\Python\Python310\python.exe: can't open file 'C:\\PyProjects\\Project_1\\main.py': [Errno 2] No such file or directory
    Process finished with exit code 2

---
+++ Lesson_2+HW/HW2_1.py
@@ -0,0 +1,8 @@
+data = [10, 23.4, 'Hello']
+data.insert(1, 'bonjour')
+print(data)
+type(data)
+text_list = ['h', 'e', 'l', 'l', 'o']
+print(type(text_list))
+text_list2 = ['hello', 1, 2, 'wealth']
+print(type(text_list2))
