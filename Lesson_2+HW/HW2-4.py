1bffd682fa15b81e349bcc8b0951187bd71e84a3
milash1 <ludmila.chitik@gmail.com>
2022-03-07 09:56:37 +0300

    Python HW2

--- Lesson_2+HW/HW2-4.py
+++
@@ -1,6 +0,0 @@
-idea = input('напишите, о чем думаете - ')
-print(type(idea))
-data = idea.split()
-print(data)
-for i, item in enumerate(data, 1):#взято из урока 3 в ответах
-  print(f'{i} - {item}')
